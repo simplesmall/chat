ac8f2a067755425066ba83bde1fd10bb4d0a8f44
guanshaochun <guanshaochun@ebupt.com>
2020-12-15 14:11:50 +0800

    Init project

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Chat from './views/Chat'
+export default {
+  name: 'App',
+  components: {
+    Chat
+  }
+}
+</script>
+
+<style scoped>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: -4px;
+    position: absolute;
+
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    overflow: hidden;
+    -webkit-backface-visibility: hidden;
+    backface-visibility: hidden;
+    transform: translate3d(0, 0, 0);
+    transform-style: preserve-3d;
+    visibility: visible;
+  }
+</style>
